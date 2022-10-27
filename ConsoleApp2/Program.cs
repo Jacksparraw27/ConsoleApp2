640895fb6a4d87e8f4025daa4407220b6d7597a4
Saiful  Islam <saifulislam270503@gmail.com>
2022-10-27 17:05:12 +0700

    memasukkan nilai mid lowerbound + upperbound

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -37,5 +37,31 @@ namespace ConsoleApp2
                 arr[i] = Int32.Parse(s1);
             }
         }
-    }
-}
+        public void BinarySearch()
+        {
+            char ch;
+            do
+            {
+                // accept the number to be searched
+                Console.Write
+                    ("\nEnter element want you to search :");
+                int item = Convert.ToInt32(Console.ReadLine());
+
+                // Apply binary search
+                int lowerbound = 0;
+                int upperbound = n - 1;
+
+                //obtain the index of the middle elements
+                int mid = (lowerbound + upperbound) / 2;
+                int ctr = 1;
+
+                //loop to search for the elements in the array
+                while ((item != arr[mid]) && (lowerbound <= upperbound))
+                {
+                    if (item > arr[mid])
+                        lowerbound = mid + 1;
+                    else
+                        upperbound = mid - 1;
+
+                    mid = (lowerbound + upperbound) / 2;
+                    ctr++;
